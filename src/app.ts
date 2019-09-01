f7e5007eb56b67231359a94abb72b9975d337062
aval501 <aval501@outlook.com>
2019-09-01 01:44:43 -0700

    completed balance and transactions api in local db

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import Net from "net";
 import uuid from "uuid/v4";
-
 import sqlite3 from "sqlite3";
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const db = new sqlite3.Database("./ledger.db", (error) => {
     if (!!error) console.error(error);
@@ -21,7 +26,7 @@ db.serialize(() => {
     db.run(`CREATE TABLE IF NOT EXISTS "transactions" (
         "id"	        TEXT,
         "initiateTime"  TEXT NOT NULL,
-        "from"	        TEXT NOT NULL,
+        "from"	        TEXT,
         "to"	        TEXT NOT NULL,
         "amount"	    NUMERIC NOT NULL,
         "requestTime"   TEXT NOT NULL,
@@ -37,24 +42,149 @@ db.serialize(() => {
         PRIMARY KEY("confirmTime", "transactionId", "requestNodeId", "confirmNodeId"));`);
 
     console.log(`Adding the first transaction if the ledger is empty.`);
-    db.each(`SELECT COUNT(*) FROM transactions`, (error, row) => {
+    db.each(`SELECT COUNT(*) FROM transactions;`, (error, row) => {
         const totalTransactions = row["COUNT(*)"];
         if (totalTransactions > 0) {
             console.log(`Transaction not empty, first transaction not needed.`);
             return;
         }
-    
+
         db.run(`INSERT INTO transactions VALUES (
             "${uuid()}",
             "${new Date().toISOString()}",
-            "system",
+            NULL,
             "system",
             1000000000,
             "${new Date().toISOString()}",
             "${new Date().toISOString()}"
-        )`);
+        );`);
     });
-})
+
+    onLedgerInitialized(app);
+});
+
+async function getBalanceAsync(username: string): Promise<{ amount: number }> {
+    username = username.toLowerCase();
+
+    return new Promise((resolve, reject) => {
+        let receiveAmount = 0;
+        let sendAmount = 0;
+
+        db.each(
+            `SELECT sum(t1.amount) as "receiveAmount",
+            (SELECT sum(t2.amount) from transactions t2 WHERE "from" = "${username}") as "sendAmount"
+            from transactions t1 WHERE "to" = "${username}";`,
+            (error, row) => {
+                if (!!row["receiveAmount"]) receiveAmount = row["receiveAmount"];
+                if (!!row["sendAmount"]) sendAmount = row["sendAmount"];
+            }, (error) => {
+                if (!!error) {
+                    reject(error);
+                    return;
+                }
+
+                resolve({
+                    amount: receiveAmount - sendAmount
+                });
+            });
+    });
+}
+
+async function getTransactionsAsync(username: string, limit: number, fromTime?: Date, toTime?: Date): Promise<any[]> {
+    username = username.toLowerCase();
+
+    return new Promise<any[]>((resolve, reject) => {
+        const rows: any[] = [];
+        // console.log(query);
+        db.each(`SELECT * from transactions WHERE "from" = "${username}" or "to" = "${username}"
+                ORDER BY confirmTime DESC LIMIT ${limit};`,
+            (error, row) => {
+                rows.push(row);
+            }, (error) => {
+                if (!!error) {
+                    console.error(error);
+                    reject(error);
+                }
+
+                resolve(rows);
+            });
+    });
+}
+
+async function postTrasactionAsync(from: string, to: string, amount: number): Promise<any> {
+    from = from.toLowerCase();
+    to = to.toLowerCase();
+
+    return new Promise(async (resolve, reject) => {
+        const balance = await getBalanceAsync(from);
+        if (balance.amount < amount) {
+            reject({
+                error: {
+                    code: "notEnoughBalance",
+                    message: `Not enough balance, amount requested: ${amount} > balance: ${balance.amount}.`
+                }
+            });
+            return;
+        }
+
+        db.run(`INSERT INTO transactions VALUES (
+            "${uuid()}",
+            "${new Date().toISOString()}",
+            "${from}",
+            "${to}",
+            ${amount},
+            "${new Date().toISOString()}",
+            "${new Date().toISOString()}");`,
+            async (error) => {
+                if (!!error) {
+                    reject(error);
+                }
+
+                const transactions = await getTransactionsAsync(from, 1);
+                resolve(transactions[0]);
+            });
+    });
+}
+
+app.get("/info/:username", async (req, res) => {
+    const username = !req.params.username ? "system" : req.params.username;
+    const balance = await getBalanceAsync(username);
+    const transactions = await getTransactionsAsync(username, 100);
+
+    res.json({
+        username: username,
+        amount: balance.amount,
+        recentTransactions: transactions,
+    });
+});
+
+app.get("/balance", async (req, res) => {
+    const username = "system";
+    const balance = await getBalanceAsync(username);
+    const transactions = await getTransactionsAsync(username, 100);
+
+    res.json({
+        username: username,
+        amount: balance.amount,
+        recentTransactions: transactions,
+    });
+});
+
+app.get("/transactions", async (req, res) => {
+    const transactions = await getTransactionsAsync("system", 10000);
+    res.json(transactions);
+});
+
+app.post("/transactions", async (req, res) => {
+    const from = "system";
+    const transaction = await postTrasactionAsync(from, req.body.to, req.body.amount);
+    res.json(transaction);
+});
+
+function onLedgerInitialized(app: any): void {
+    console.log(`Server listing at port: 60000... Try 'localhost:60000/balance'.`);
+    app.listen(60000);
+}
 
 // let port = 60001;
 
